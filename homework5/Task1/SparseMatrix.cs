d2b84427d682e5e09500daceb54cba429d5ffd15
Meteorych <super.titlov@inbox.ru>
2023-11-28 01:05:30 +0300

    First task is completed

--- homework5/Task1/SparseMatrix.cs
+++ homework5/Task1/SparseMatrix.cs
@@ -1,35 +1,37 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections;
+using System.Text;
 
 namespace Task1
 {
-    public class SparseMatrix
+    public class SparseMatrix : IEnumerable<long>
     {
         private readonly int _columns;
         private readonly int _rows;
         private Dictionary<(int, int), long> _data;
         
-        public SparseMatrix(int columns, int rows)
+        public SparseMatrix(int rows, int columns)
         {
             if (columns <= 0 || rows <= 0)
             {
-                throw new ArgumentException("Wrong arguments!");
+                throw new ArgumentException("Wrong arguments! Rows and columns should be greater that zero.");
             }
             _columns = columns;
             _rows = rows;
             _data = new Dictionary<(int, int), long>();
         }
 
-        public long this[int column, int row]
+        public long this[int row, int column]
         {
             set
             {
-                CheckIndices(column, row);
-                _data.Add((column, row), value);
+                CheckIndices(row, column);
+                _data.Add((row, column), value);
             }
             get
             {
-                CheckIndices(column, row);
-                return _data.ContainsKey((column, row)) ? _data[(column, row)] : 0L;
+                CheckIndices(row, column);
+                return _data.ContainsKey((row, column)) ? _data[(row, column)] : 0L;
             }
         }
 
@@ -40,9 +42,12 @@ namespace Task1
             {
                 for (var j = 0; j < _columns; j++)
                 {
-                    if (_data.ContainsKey((j, i)))
+                    //Очевидна идея использовать просто индексатор при построении строки, но я предполагаю, что таким образом программа будет исполняться быстрее, как минимум из-за остутствия постоянных
+                    //обращений к CheckIndices и тесты показали небольшой выигрыш в миллисекундах, поэтому решился на такой эксперимент в данном конкретном случае, заодно интересно
+                    //что вы считает насчёт подобной реализации.
+                    if (_data.ContainsKey((i, j)))
                     {
-                        resultBuilder.Append($" {_data[(j, i)]} ");
+                        resultBuilder.Append($" {_data[(i, j)]} ");
                     }
                     else
                     {
@@ -54,19 +59,48 @@ namespace Task1
             return resultBuilder.ToString();
         }
 
-        private void CheckIndices(int index1, int index2)
-        {
-            Check(index1, nameof(index1));
-            Check(index2, nameof(index2));
-            return;
 
-            void Check(int index, string indexName)
+
+        public IEnumerator<long> GetEnumerator()
+        {
+            for (var i = 0; i < _rows; i++)
             {
-                if (index <= 0 || index > _columns * _rows)
+                for (var j = 0; j < _columns; j++)
                 {
-                    throw new ArgumentOutOfRangeException(nameof(indexName));
+                    yield return this[i, j];
                 }
             }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerable<(int, int, long)> GetNonZeroElements()
+        {
+            var sortedElements = _data.OrderBy(pair => pair.Key.Item2).ThenBy(pair => pair.Key.Item1);
+            foreach (var el in sortedElements)
+            {
+                yield return (el.Key.Item1, el.Key.Item2, el.Value);
+            }
+        }
+
+        public int GetCount(long x)
+        {
+            if (x == 0) return _columns * _rows - _data.Count;
+            var occurrencesOfValue = _data.Where(pair => pair.Value == x);
+            return occurrencesOfValue.Count();
+
+        }
+
+        private void CheckIndices(int row, int column)
+        {
+            if (row < 0 || row >= _rows || column < 0 || column >= _columns) 
+            {
+                throw new ArgumentOutOfRangeException(nameof(row));
+            }
+            
         }
 
     }
