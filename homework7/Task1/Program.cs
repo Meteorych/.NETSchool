b711e7d1bb68101af5d8313881ca072321046c62
Meteorych <super.titlov@inbox.ru>
2023-12-17 18:15:26 +0300

    Task1

---
+++ homework7/Task1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Xml.Linq;
+
+namespace Task1;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		var vacations = new List<Vacation>();
+		try
+		{
+			using (var dataStream = new StreamReader(Path.Combine(Environment.CurrentDirectory, "data.csv")))
+			{
+				string line;
+				var lineNumber = 0;
+				while (!string.IsNullOrEmpty(line = dataStream.ReadLine()))
+				{
+					var vacation = new Vacation();
+					var parts = line.Split(',');
+					lineNumber++;
+					if (parts.Length != 3)
+					{
+						Console.WriteLine($"Mistake in the string number {lineNumber}");
+						continue;
+					}
+
+					vacation.EmployeeName = parts[0].Trim();
+					if (!DateTime.TryParseExact(parts[1].Trim(), "M/d/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out var beginDate))
+					{
+						Console.WriteLine($"Wrong beginDate format in the string number {lineNumber}");
+						continue;
+					}
+
+					vacation.BeginDate = beginDate;
+					if (!DateTime.TryParseExact(parts[2].Trim(), "M/d/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out var endDate))
+					{
+						Console.WriteLine($"Wrong endDate format in the string number {lineNumber}");
+						continue;
+					}
+
+					vacation.EndDate = endDate;
+					vacations.Add(vacation);
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			Console.WriteLine($"{e}");
+		}
+		AverageVacationSpan(vacations);
+		VacationEmployee(vacations);
+	}
+
+	public static double AverageVacationSpan(List<Vacation> vacations)
+	{
+		var result = vacations
+			.Select(vacation => (vacation.EndDate - vacation.BeginDate).Days + 1)
+			.Average();
+		return result; 
+	}
+
+	public static void VacationEmployee(List<Vacation> vacations)
+	{
+		var result = vacations
+			.GroupBy(vacation => vacation.EmployeeName)
+			.Select(employeeVacations => new
+			{
+				employeeName = employeeVacations.Key,
+				TotalVacationDays = employeeVacations.Sum(g => (g.EndDate - g.BeginDate).Days + 1)
+			})
+			.OrderByDescending(employee => employee.TotalVacationDays);
+		ToJson(result);
+		foreach (var element in result)
+		{
+			Console.WriteLine(element);
+		}
+	}
+
+	public static void ToJson(IOrderedEnumerable<object> vacationDuration)
+	{
+		var json = JsonSerializer.Serialize(vacationDuration, new JsonSerializerOptions() { WriteIndented = true });
+		File.WriteAllText("data.json", json);
+	}
+}
---
+++ homework7/Task1/Vacation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+	public class Vacation
+	{
+		public string EmployeeName { get; set; }
+
+		public DateTime BeginDate { get; set; }
+		public DateTime EndDate { get; set; }
+
+		public Vacation()
+		{
+			
+		}
+	}
+}
